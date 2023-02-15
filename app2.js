57452179a230be9b4e0393f7e3b4c7423e15c7bb
mydds <125189411+mydds@users.noreply.github.com>
2023-02-15 12:59:17 +0800

    Update app2.js
    adding apostrophe in Like I'm label

--- app2.js
+++ app2.js
@@ -23,7 +23,7 @@ firebase.database().ref('LikeIm').on('value', function(snapshot) {
 // Listen for onchange event of ExplainMenu dropdown and update LikeImMenu options
 document.getElementById("ExplainMenu").onchange = function() {
     let selectedExplain = this.value;
-    let options = '<option value="">Like Im:</option>';
+    let options = '<option value="">Like I\'m:</option>';
     if (!selectedExplain) {
         // If ExplainMenu selection is empty, show all options in LikeImMenu
         firebase.database().ref('LikeIm').on('value', function(snapshot) {
